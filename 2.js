252053758f1658ed1e729aadfbc163dc3ca2cdb8
zhenyanenado <70278943+zhenyanenado@users.noreply.github.com>
2022-01-03 11:05:18 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', (event)=>{
+
+	let c = console.log;
+	let inp1 = document.getElementById('inp1');
+
+
+		window.addEventListener('touchstart', (event)=>{
+
+			let pos = event.target;
+			let value = inp1.value;
+
+			let posX = event.touches[0].clientX;
+
+
+
+				if(posX >= 740){
+
+					value = "";
+					
+
+				}
+
+				if(pos == inp1){
+					inp1.style.backgroundPosition = '-20%';
+				}
+
+				else {
+
+					inp1.value = "";
+
+				}
+
+
+
+		})
+
+
+
+
+})
